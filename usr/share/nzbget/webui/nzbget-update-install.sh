c6f1c25f04a5e60549f3eae123c9c319cf064638
cptjhmiller <cpt.miller@fun4gamers.co.uk>
2015-02-07 15:57:20 +0000

    Complete update settings
    Update works but does not restart nzbget afterwards????????

--- usr/share/nzbget/webui/nzbget-update-install.sh
+++ usr/share/nzbget/webui/nzbget-update-install.sh
@@ -1,49 +1,44 @@
 #!/bin/sh
-#
 mkdir -p /tmp/nzbget
+chmod 775 /tmp/nzbget
 cd /tmp/nzbget
+
+URL="http://jamied.pwp.blueyonder.co.uk/nzbget/nzbget.${NZBUP_BRANCH}.tar.gz"
+
 echo "Installing ${NZBUP_BRANCH}"
-echo "Downloading new version...";
-wget -q "http://jamied.pwp.blueyonder.co.uk/nzbget/nzbget.${NZBUP_BRANCH}.tar.gz"
 
-if [ -f "/tmp/nzbget/nzbget.${NZBUP_BRANCH}.tar.gz" ]; then
-    echo "Downloaded new version...OK"
-    echo "Installing Updated Version."
-    tar -zxf "/tmp/nzbget/nzbget.${NZBUP_BRANCH}.tar.gz"
-    arch=$(uname -m | sed 's/x86_//;s/i[3-6]86/32/')
-    if [ $arch -eq 32 ]; then
-        echo "Installing binary file for a 32 bit systems"
-        mv nzbget_i386 /opt/nzbget/
-    elif [ $arch -eq 64 ]; then
-        echo "Installing binary file for a 64 bit systems"
-        mv nzbget_amd64 /opt/nzbget/
-    else
-        echo "Installing binary file for a arm type systems"
-        mv nzbget_arm /opt/nzbget/
-    fi
+echo "Downloading new version..."
+wget -q --no-check-certificate "${URL}" || echo "[ERROR] Download failed" | exit 1
+echo "Downloading new version...OK";
+tar -zxf "/tmp/nzbget/nzbget.${NZBUP_BRANCH}.tar.gz"
+chmod 775 /tmp/nzbget -R
+chown nzbget:users /tmp/nzbget -R
+cd /
 
-    if [ -f "/opt/nzbget/nzbget" ]; then
-        rm /opt/nzbget/nzbget
-    fi
-	
-    mv /opt/nzbget/nzbget_* /opt/nzbget/nzbget
-
-    chown nzbget:users /opt/nzbget/nzbget
-    rm -Rf /usr/share/nzbget/webui
-    mv webui /usr/share/nzbget/
-    sleep 2
-    rm -R /tmp/nzbget
+# Installing the update
+arch=$(uname -m | sed 's/x86_//;s/i[3-6]86/32/')
+if [ $arch -eq 32 ]; then
+    echo "Installing binary file for a 32 bit systems"
+    mv /tmp/nzbget/nzbget_i386 /opt/nzbget/
+elif [ $arch -eq 64 ]; then
+    echo "Installing binary file for a 64 bit systems"
+    mv /tmp/nzbget/nzbget_amd64 /opt/nzbget/
 else
-    rm -R /tmp/nzbget
-    echo "[ERROR] Download failed, try again later"
-    sleep 10
-    exit 0
+    echo "Installing binary file for a arm type systems"
+    mv /tmp/nzbget/nzbget_arm /opt/nzbget/
 fi
-#Stop NZBGet Daemon
-echo -n "Stoping NZBGet Daemon : "
-service nzbget stop
-sleep 2
-echo "Starting NZBGet..."
-service nzbget start
-#$NZBOP_APPBIN -c $NZBOP_CONFIGFILE -D
+
+if [ -f "/opt/nzbget/nzbget" ]; then
+    rm /opt/nzbget/nzbget
+fi
+
+echo "Restarting NzbGet..."
+mv /opt/nzbget/nzbget_* /opt/nzbget/nzbget
+chown nzbget:users /opt/nzbget/nzbget
+rm -Rf /usr/share/nzbget/webui
+mv /tmp/nzbget/webui /usr/share/nzbget/
+
+rm -Rf /tmp/nzbget
+sudo -u root /etc/init.d/nzbget restart >/dev/null 2>&1
+echo "Update Completed, close window and refresh the page."
 exit 0
